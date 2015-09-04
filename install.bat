ad0ef2399421f4861d856d5ac9c0e84f1e98225e
Martin Valgur <martin.valgur@gmail.com>
2015-09-04 18:07:22 +0300

    Add scripts for packaging and installation

---
+++ install.bat
@@ -0,0 +1,1 @@
+call "%~dp0target\esa-snap_windows-x64_nightly.exe" -q -console -dir "C:\Program Files\snap"
---
+++ package.bat
@@ -0,0 +1,2 @@
+call updateall.bat
+call "C:\Program Files\install4j5\bin\install4jc.exe" "%~dp0snap.install4j" --verbose --faster --build-ids=56 --release=nightly
